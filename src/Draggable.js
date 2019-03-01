54660bb4f73654a647118152ffd000ef90f3d25b
acolicc <acolicc@yahoo.com>
2019-03-01 15:37:41 -0500

    update to not show modal until dragging begins

--- src/Draggable.js
+++ src/Draggable.js
@@ -12,40 +12,41 @@ import {
 import PropTypes from 'prop-types';
 
 class Draggable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'Draggable';
-        this._initiateDrag = this._initiateDrag.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.displayName = 'Draggable';
+    this._initiateDrag = this._initiateDrag.bind(this);
+  }
 
-    static contextTypes = {
-      dragContext: PropTypes.any
-    }
+  static contextTypes = {
+    dragContext: PropTypes.any
+  }
 
-    static propTypes = {
-      dragOn: PropTypes.oneOf(['onLongPress', 'onPressIn'])
-    }
+  static propTypes = {
+    dragOn: PropTypes.oneOf(['onLongPress', 'onPressIn'])
+  }
 
-    _initiateDrag() {
-      if (!this.props.disabled) this.context.dragContext.onDrag(this.refs.wrapper, this.props.children, this.props.data);
-    }
+  _initiateDrag() {
+    if (!this.props.disabled) this.context.dragContext.onDrag(this.refs.wrapper, this.props.children, this.props.data);
+  }
 
-    static defaultProps = {
-      dragOn: 'onLongPress'
-    }
+  static defaultProps = {
+    dragOn: 'onLongPress'
+  }
 
-    render() {
-        
-        let isDragging = this.context.dragContext.dragging && this.context.dragContext.dragging.ref;
-        isDragging = isDragging && isDragging === this.refs.wrapper;
-        return <TouchableOpacity activeOpacity={this.props.activeOpacity} style={this.props.style} onLongPress={this.props.dragOn === 'onLongPress' ? this._initiateDrag : null}  onPress={this.props.onPress} onPressIn={this.props.dragOn === 'onPressIn' ? this._initiateDrag : null} ref="wrapper">
-        {
-          React.Children.map(this.props.children, child => {
-          return React.cloneElement(child, {ghost: isDragging})
+  render() {
+
+    let isDragging = this.context.dragContext.dragging && this.context.dragContext.dragging.ref;
+    isDragging = isDragging && isDragging === this.refs.wrapper;
+    let didBeginDragging = this.context.dragContext.didBeginDragging;
+    return <TouchableOpacity activeOpacity={this.props.activeOpacity} style={this.props.style} onLongPress={this.props.dragOn === 'onLongPress' ? this._initiateDrag : null} onPress={this.props.onPress} onPressIn={this.props.dragOn === 'onPressIn' ? this._initiateDrag : null} ref="wrapper">
+      {
+        React.Children.map(this.props.children, child => {
+          return React.cloneElement(child, { ghost: isDragging && didBeginDragging })
         })
-        }
-      </TouchableOpacity>;
-    }
+      }
+    </TouchableOpacity>;
+  }
 }
 
 export default Draggable;
